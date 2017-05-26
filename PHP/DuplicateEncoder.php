f01a68da5e47c42897b26aa0facda82c7943ccfc
Jessynt <xr5299@gmail.com>
2017-05-26 19:02:43 +0800

    Duplicate Encoder

---
+++ PHP/DuplicateEncoder.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/common.php';
+// The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples:
+
+// "din" => "((("
+
+// "recede" => "()()()"
+
+// "Success" => ")())())"
+
+// "(( @" => "))(("
+function duplicate_encode($word) 
+{
+    $chars = str_split(strtolower($word));
+    $result = [];
+    foreach ($chars as $key => $word) {
+		$result[] = count(array_keys($chars, $word)) === 1 ? '(' : ')';
+    }
+    return implode($result, '');
+}
+
+dd(duplicate_encode("iiii#@"));
